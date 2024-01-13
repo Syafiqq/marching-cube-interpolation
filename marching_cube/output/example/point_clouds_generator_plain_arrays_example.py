c74e00b7b45e81c8fc4461c4e308be76a6e49d85
msyafiqq <muhammad.syafiq@geniebook.com>
2024-01-13 21:08:56 +0700

    update

--- marching_cube/output/example/point_clouds_generator_plain_arrays_example.py
+++ marching_cube/output/example/point_clouds_generator_plain_arrays_example.py
@@ -1,7 +1,7 @@
 from marching_cube.domain.point_clouds import point_clouds_generator_plain_arrays
 
 
-def example_sq_object_from_sanitised_txt():
+def example():
     input_path = '../../input/resource/plain/sq_object_sanitised.txt'
     xy_size = 21
     z_size = 8
