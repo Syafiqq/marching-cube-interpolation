03a847ab49627338e7fd013cab83af23dc6b9bfb
msyafiqq <muhammad.syafiq@geniebook.com>
2024-03-10 22:21:24 +0700

    SFQ-2: add output example

---
+++ marching_cube/output/example/point_clouds_generator_plain_arrays_example.py
@@ -0,0 +1,14 @@
+from marching_cube.domain.point_clouds import point_clouds_generator_plain_arrays
+
+
+def example():
+    input_path = '../../input/resource/plain/sq_object_sanitised.txt'
+    xy_size = 21
+    z_size = 8
+
+    _, metadata = point_clouds_generator_plain_arrays.generate_by_input(input_path, xy_size, z_size)
+    print(metadata)
+
+
+if __name__ == "__main__":
+    pass
